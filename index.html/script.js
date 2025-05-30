6401d5ccae8131f58663edf0b5cd29610affd0da
Yahks <elearningyahaya@gmail.com>
2025-05-30 11:19:58 +0100

    Add files via upload

--- index.html/script.js
+++ index.html/script.js
@@ -1,6 +1,3 @@
- const menuIcon = document.querySelector('.menu-icon');
-    const navbar = document.querySelector('.navbar');
-
-    menuIcon.addEventListener('click', () => {
-      navbar.classList.toggle('show');
-    });
+ function toggleMenu() {
+      document.getElementById('menu-list').classList.toggle('show');
+    }
